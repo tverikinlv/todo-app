1f0efa5cf4f680dccf41747135c650bf4df93dd3
LeoVT <tverikin.leonid@yandex.ru>
2023-08-23 15:05:53 +0300

    filters added

--- src/components/todo-section/todo-section.tsx
+++ src/components/todo-section/todo-section.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './todo-section.css';
 
 
@@ -7,6 +7,8 @@ export default function TodoSection() {
 
     const [text, setText] = useState<string>('');
     const [todos, setTodos] = useState<{ id: string, text: string, completed: boolean }[]>([]);
+    const [radioId, setRadioId] = useState<string>('all');
+    const [filteredTodos, setFilteredTodos] = useState<{ id: string, text: string, completed: boolean }[]>([]);
     const inputRef = useRef<HTMLInputElement>(null);
 
     const handleKeyDown = (event: { key: string; }) => {
@@ -31,26 +33,47 @@ export default function TodoSection() {
         }
     }
 
-    const removeTodo = (todoId:string) => {
-        setTodos(todos.filter( todo => todo.id !== todoId)) 
+    const removeTodo = (todoId: string) => {
+        setTodos(todos.filter(todo => todo.id !== todoId))
     }
 
-    const toggleComplete = (todoId:string) => {
+    const toggleComplete = (todoId: string) => {
         setTodos(
-            todos.map( todo => {
-                if(todo.id !== todoId) return todo;
-                
+            todos.map(todo => {
+                if (todo.id !== todoId) return todo;
+
                 return {
-                    ...todo, 
-                    completed: ! todo.completed
+                    ...todo,
+                    completed: !todo.completed
                 }
-            }) 
+            })
         )
     }
 
     const clearCompleted = () => {
-        setTodos(todos.filter( todo => todo.completed === false))
+        setTodos(todos.filter(todo => todo.completed === false))
     }
+
+    const filterTodos = () => {
+        if (radioId === 'all') setFilteredTodos(todos)
+
+        if (radioId === 'active')
+            setFilteredTodos(
+                todos.filter(
+                    todo => todo.completed === false
+                )
+            )
+
+        if (radioId === 'completed')
+            setFilteredTodos(
+                todos.filter(
+                    todo => todo.completed === true
+                )
+            )
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => filterTodos(), [todos, radioId])
 
     return (
         <section className='todo'>
@@ -67,30 +90,51 @@ export default function TodoSection() {
                 </label>
             </div>
             <div>
-                <ul className='todo__list'>
-                    {todos.map((todo) =>
-                        <li key={todo.id} className='todo__item' draggable={true}>
-                            <input 
-                                type='checkbox' 
-                                id={todo.id} 
-                                className='todo__item-checkbox' 
-                                checked={todo.completed} 
-                                onChange={() => toggleComplete(todo.id)}/>
-                            <label htmlFor={todo.id}>{todo.text}</label>
-                            <button className='todo__close-button' onClick={()=>removeTodo(todo.id)}></button>
-                        </li>)
-                    }
-                </ul>
-                <div className='todo__total'>
-                    <span>{todos.length} items left</span>
-                    <button className='todo__clear-button' onClick={clearCompleted}>Clear Completed</button>
+                <div className="todo__list">
+                    <ul>
+                        {filteredTodos.map((todo) =>
+                            <li key={todo.id} className='todo__item' draggable={true}>
+                                <input
+                                    type='checkbox'
+                                    id={todo.id}
+                                    className='todo__item-checkbox'
+                                    checked={todo.completed}
+                                    onChange={() => toggleComplete(todo.id)} />
+                                <label htmlFor={todo.id}>{todo.text}</label>
+                                <button className='todo__close-button' onClick={() => removeTodo(todo.id)}></button>
+                            </li>)
+                        }
+
+                    </ul>
+                    <div className='todo__total'>
+                        <span>{filteredTodos.length} items left</span>
+                        <button className='todo__clear-button' onClick={clearCompleted}>Clear Completed</button>
+                    </div>
                 </div>
                 <div className="todo__filters-panel">
-                    <input type="radio" className='todo__filter' id="all" name='filter'/>
+                    <input
+                        type="radio"
+                        className='todo__filter'
+                        id="all"
+                        name='filter'
+                        checked={radioId === 'all'}
+                        onChange={() => setRadioId('all')} />
                     <label htmlFor="all">All</label>
-                    <input type="radio" className='todo__filter' id="active" name='filter'/>
+                    <input
+                        type="radio"
+                        className='todo__filter'
+                        id="active"
+                        name='filter'
+                        checked={radioId === 'active'}
+                        onChange={() => setRadioId('active')} />
                     <label htmlFor="active">Active</label>
-                    <input type="radio" className='todo__filter' id="completed" name='filter'/>
+                    <input
+                        type="radio"
+                        className='todo__filter'
+                        id="completed"
+                        name='filter'
+                        checked={radioId === 'completed'}
+                        onChange={() => setRadioId('completed')} />
                     <label htmlFor="completed">Completed</label>
                 </div>
             </div>
